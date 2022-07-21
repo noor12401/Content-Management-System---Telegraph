bc96a245b61ce56345dc276a97efbbe7ef14c4ff
Noor <nooruddin12401@gmail.com>
2022-07-21 23:13:12 +0530

    Toggling Dark and Light Mode

--- client/components/ToggleTheme.js
+++ client/components/ToggleTheme.js
@@ -2,6 +2,7 @@ import { useContext } from "react";
 import { ThemeContext } from "../context/theme";
 import Head from "next/head";
 
+//Creating state to toggle dark and ligh mode
 const ToggleTheme = () => {
   const [theme, setTheme] = useContext(ThemeContext);
 
@@ -11,25 +12,49 @@ const ToggleTheme = () => {
         <link rel="stylesheet" href={`/css/${theme}.css`} />
       </Head>
       {theme === "light" ? (
-        <span
+        //SVG Icon for dark mode
+        <svg
           onClick={() => {
             setTheme("dark");
             localStorage.setItem("theme", "dark");
           }}
-          style={{ fontSize: "24px" }}
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-gray-900"
+          fill="none"
+          stroke="currentColor"
+          width="50"
+          height="50"
+          viewBox="0 0 24 24"
         >
-          🌓
-        </span>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+          />
+        </svg>
       ) : (
-        <span
+        //SVG Icon for light mode
+        <svg
           onClick={() => {
             setTheme("light");
             localStorage.setItem("theme", "light");
           }}
-          style={{ fontSize: "24px" }}
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 text-indigo-200"
+          fill="none"
+          stroke="currentColor"
+          width="50"
+          height="50"
+          viewBox="0 0 24 24"
         >
-          🌞
-        </span>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+          />
+        </svg>
       )}
     </>
   );
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,4 @@
 import { ThemeProvider } from "../context/theme";
-// import "antd/dist/antd.css";
-import "antd/dist/antd.dark.css";
 
 function MyApp({ Component, pageProps }) {
   return (
