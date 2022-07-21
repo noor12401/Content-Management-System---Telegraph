397253b45246b17b200851aee8ddb60855162747
shaarifg <ermohdsharif@gmail.com>
2022-07-21 20:43:45 +0530

    adding context api

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,12 @@
-function Home () {
-    return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
-    )
+import { Button } from "antd";
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Button type="primary">Click me</Button>
+    </div>
+  );
 }
 
 export default Home;
