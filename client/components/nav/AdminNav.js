9aabe83d363e5c97be8143a6c3e6722c583b888f
Noor <nooruddin12401@gmail.com>
2022-07-23 01:46:42 +0530

    Adding Icons

--- client/components/nav/AdminNav.js
+++ client/components/nav/AdminNav.js
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import { Menu, Button, Layout } from "antd";
 import Link from "next/link";
 import {
-  AppstoreOutlined,
-  MenuUnfoldOutlined,
-  MenuFoldOutlined,
   PieChartOutlined,
-  DesktopOutlined,
-  ContainerOutlined,
   MailOutlined,
+  PushpinOutlined,
+  CameraOutlined,
+  UsergroupAddOutlined,
+  PoweroffOutlined,
+  BgColorsOutlined,
+  UserOutlined,
+  CommentOutlined,
 } from "@ant-design/icons";
 
 const { SubMenu } = Menu;
@@ -21,29 +23,29 @@ const AdminNav = () => {
     <Sider collapsible>
       <Menu
         defaultSelectedKeys={["1"]}
-        defaultOpenKeys={["sub1", "sub2"]}
+        defaultOpenKeys={["2"]}
         mode="inline"
         inlineCollapsed={collapsed}
       >
-        <Menu.Item key="1" icon={<PieChartOutlined />}>
+        <Menu.Item key="1" icon={<PoweroffOutlined />}>
           <Link href="/admin">
             <a>Dashboard</a>
           </Link>
         </Menu.Item>
 
         {/* posts */}
-        <SubMenu key="sub1" icon={<MailOutlined />} title="Posts">
-          <Menu.Item key="2">
+        <SubMenu key="2" icon={<PushpinOutlined />} title="Posts">
+          <Menu.Item key="3">
             <Link href="/admin/posts">
               <a>All Posts</a>
             </Link>
           </Menu.Item>
-          <Menu.Item key="3">
+          <Menu.Item key="4">
             <Link href="/admin/posts/new">
               <a>Add New</a>
             </Link>
           </Menu.Item>
-          <Menu.Item key="3">
+          <Menu.Item key="5">
             <Link href="/admin/categories">
               <a>Categories</a>
             </Link>
@@ -51,13 +53,13 @@ const AdminNav = () => {
         </SubMenu>
 
         {/* library */}
-        <SubMenu key="sub1" icon={<MailOutlined />} title="Media">
-          <Menu.Item key="5">
+        <SubMenu key="6" icon={<CameraOutlined />} title="Media">
+          <Menu.Item key="7">
             <Link href="/admin/media/library">
               <a>Library</a>
             </Link>
           </Menu.Item>
-          <Menu.Item key="5">
+          <Menu.Item key="8">
             <Link href="/admin/media/new">
               <a>Add New</a>
             </Link>
@@ -65,20 +67,20 @@ const AdminNav = () => {
         </SubMenu>
 
         {/* comments */}
-        <Menu.Item key="5">
+        <Menu.Item key="9" icon={<CommentOutlined />}>
           <Link href="/admin/comments">
             <a>Comments</a>
           </Link>
         </Menu.Item>
 
         {/* users */}
-        <SubMenu key="sub1" icon={<MailOutlined />} title="Users">
-          <Menu.Item key="5">
+        <SubMenu key="10" icon={<UsergroupAddOutlined />} title="Users">
+          <Menu.Item key="11">
             <Link href="/admin/users">
               <a>All Users</a>
             </Link>
           </Menu.Item>
-          <Menu.Item key="5">
+          <Menu.Item key="12">
             <Link href="/admin/users/new">
               <a>Add New</a>
             </Link>
@@ -86,14 +88,14 @@ const AdminNav = () => {
         </SubMenu>
 
         {/* profile */}
-        <Menu.Item key="5">
+        <Menu.Item key="5" icon={<UserOutlined />}>
           <Link href="/admin/userid">
             <a>Profile</a>
           </Link>
         </Menu.Item>
 
         {/* Customize */}
-        <Menu.Item key="5">
+        <Menu.Item key="5" icon={<BgColorsOutlined />}>
           <Link href="/admin/customize">
             <a>Customize</a>
           </Link>
